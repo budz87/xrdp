90417c8cafed9994d620392c349d3ce315db3fe3
budz87 <107797222+budz87@users.noreply.github.com>
2022-07-06 19:39:15 +0000

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ###################################################################################################################
-usage: cd /tmp/ && wget -O xrdp && chmod +x xrdp && ./xrdp
+usage: cd /tmp/ && wget https://raw.githubusercontent.com/budz87/xrdp/main/xrdp.sh -O xrdp && chmod +x xrdp && ./xrdp
 ###################################################################################################################
 
 install_xrdp(){
